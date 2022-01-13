1a7aa2aa2417ab2f0825a31174f7f980400a4c4d
Leonardo Vencovsky <leonardo.vencovsky@gmail.com>
2022-01-13 17:19:18 -0300

    .NET 5 Docker

--- 2022/C#/Catalog/README.md
+++ 2022/C#/Catalog/README.md
@@ -5,8 +5,34 @@
 * Docker
 * Secret Manager
 
-Start Mongo Docker
+## Start Mongo Docker
 
 ```
 docker run -d --rm --name mongo -p 27017:27017 -v mongodbdata:/data/db -e MONGO_INITDB_ROOT_USERNAME=mongoadmin -e MONGO_INITDB_ROOT_PASSWORD=Pass#word1 mongo
+```
+
+## Build docker image from project
+
+```
+docker build -t catalog:v1 .
+```
+
+## Create a docker network
+
+```
+docker network create net5tutorial
+```
+
+## Run network
+
+* Database
+
+```
+docker run -d --rm --name mongo -p 27017:27017 -v mongodbdata:/data/db -e MONGO_INITDB_ROOT_USERNAME=mongoadmin -e MONGO_INITDB_ROOT_PASSWORD=Pass#word1 --network=net5tutorial mongo
+```
+
+* Api
+
+```
+docker run --rm -p 8080:80 -e MongoDbSettings:Host=mongo -e MongoDbSettings:Password=Pass#word1 --network=net5tutorial catalog_v1
 ```
--- 2022/C#/Catalog/Startup.cs
+++ 2022/C#/Catalog/Startup.cs
@@ -1,9 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mime;
 using System.Text.Json;
-using System.Threading.Tasks;
 using AutoMapper;
 using Catalog.Repositories;
 using Catalog.Settings;
@@ -11,12 +9,9 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
@@ -91,7 +86,10 @@ namespace Catalog
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog v1"));
             }
 
-            app.UseHttpsRedirection();
+            if (env.IsDevelopment())
+            {
+                app.UseHttpsRedirection();
+            }
 
             app.UseRouting();
 
