429fb58ab5ed746644ce29a44d02eaf38afc932a
Leonardo Vencovsky <lvenk26@gmail.com>
2023-03-28 17:12:02 +0200

    Startup script fix

--- projects/DevOps/terraform/aws_vpc/startup.sh
+++ projects/DevOps/terraform/aws_vpc/startup.sh
@@ -5,6 +5,6 @@ systemctl start httpd
 systemctl enable httpd
 INTERFACE=$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/)
 SUBNETID=$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/${INTERFACE}/subnet-id)
-INTANCEID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+INSTANCEID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 echo '<html><center><h1>This instance INSTANCEID is in the subnet wih ID: SUBNETID </h1></center></html>' > /var/www/html/index.txt
 sed "s/SUBNETID/$SUBNETID/;s/INSTANCEID/$INSTANCEID/" /var/www/html/index.txt > /var/www/html/index.html
