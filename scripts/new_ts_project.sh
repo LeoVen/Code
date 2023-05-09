2e75096cf615023d335438d2709ad7d1f87132b5
Leonardo Vencovsky <lvenk26@gmail.com>
2023-05-09 14:56:37 +0200

    new scripts folder and minio

---
+++ scripts/new_ts_project.sh
@@ -0,0 +1,11 @@
+# change projectName
+mkdir projectName && cd $_
+
+npm init --yes
+npm i -D typescript ts-node
+
+./node_modules/.bin/tsc --init
+echo 'console.log("Hello, World!")' > index.ts
+
+# put this in scripts
+node --loader ts-node/esm index.ts
